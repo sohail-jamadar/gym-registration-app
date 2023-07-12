428e4320931561aef4c1287577ca4eaa7c6a6298
Sayed Sohail Jamadar <74167090+sohail-jamadar@users.noreply.github.com>
2023-07-12 14:40:21 +0000

    Implementing register form

---
+++ src/app/create-registration/create-registration.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-registration',
+  templateUrl: './create-registration.component.html',
+  styleUrls: ['./create-registration.component.scss']
+})
+export class CreateRegistrationComponent {
+
+  public packages: string[] = ['Monthly', 'Quarterly', 'Yearly'];
+  public genders: string[] = ['Male', 'Female'];
+  public importantList: string[] = [
+    "Toxic Fat Reduction",
+    "Energy and Endurance",
+    "Building Lean Muscle",
+    "Healthier Digestive System",
+    "Sugar Craving Body",
+    "Fitness",
+  ];
+
+}
